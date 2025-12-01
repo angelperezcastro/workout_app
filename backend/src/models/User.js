f3e467a03a370c1e7faa793d73520afb28784288
Ángel Pérez Castro <aperezcastro@safareyes.com>
2025-12-02 00:04:06 +0100

    Created User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  name: { type: String }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;      // 👈 MUY IMPORTANTE
