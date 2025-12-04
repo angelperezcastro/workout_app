428019e9fd1ea85f516f4940f627bc97987f895a
Ángel Pérez Castro <aperezcastro@safareyes.com>
2025-12-04 18:15:37 +0100

    Updated seedExercises.js

--- backend/seedExercises.js
+++ backend/seedExercises.js
@@ -5,26 +5,96 @@ const Exercise = require("./src/models/Exercise");
 
 const exercises = [
   // --- Pierna ---
-  { name: "Squats", muscleGroup: "Legs", type: "Strength" },
-  { name: "Quads Extensions", muscleGroup: "Legs", type: "Strength" },
-  { name: "Hamstrings Curl", muscleGroup: "Legs", type: "Strength" },
-  { name: "Romanian Deadlift", muscleGroup: "Legs", type: "Strength" },
+  {
+    name: "Squats",
+    muscleGroup: "Legs",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164128.png",
+  },
+  {
+    name: "Quads Extensions",
+    muscleGroup: "Legs",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164135.png",
+  },
+  {
+    name: "Hamstrings Curl",
+    muscleGroup: "Legs",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164141.png",
+  },
+  {
+    name: "Romanian Deadlift",
+    muscleGroup: "Legs",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164105.png",
+  },
 
   // --- Pecho ---
-  { name: "Bench Press", muscleGroup: "Chest", type: "Strength" },
-  { name: "Upper Chest Flies", muscleGroup: "Chest", type: "Strength" },
-  { name: "Lower Chest Flies", muscleGroup: "Chest", type: "Strength" },
-  { name: "Peck Deck", muscleGroup: "Chest", type: "Strength" },
+  {
+    name: "Bench Press",
+    muscleGroup: "Chest",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 163954.png",
+  },
+  {
+    name: "Upper Chest Flies",
+    muscleGroup: "Chest",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164151.png",
+  },
+  {
+    name: "Lower Chest Flies",
+    muscleGroup: "Chest",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164201.png",
+  },
+  {
+    name: "Peck Deck",
+    muscleGroup: "Chest",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164800.png",
+  },
 
   // --- Espalda ---
-  { name: "Pull Down", muscleGroup: "Back", type: "Strength" },
-  { name: "Row Machine", muscleGroup: "Back", type: "Strength" },
-  { name: "Standing Row", muscleGroup: "Back", type: "Strength" },
-  { name: "Cable Pull Over", muscleGroup: "Back", type: "Strength" },
+  {
+    name: "Pull Down",
+    muscleGroup: "Back",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164807.png",
+  },
+  {
+    name: "Row Machine",
+    muscleGroup: "Back",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164817.png",
+  },
+  {
+    name: "Standing Row",
+    muscleGroup: "Back",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164120.png",
+  },
+  {
+    name: "Cable Pull Over",
+    muscleGroup: "Back",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164826.png",
+  },
 
   // --- Brazos / Hombros ---
-  { name: "Barbell Biceps Curl", muscleGroup: "Arms", type: "Strength" },
-  { name: "Lateral Raises", muscleGroup: "Shoulders", type: "Strength" },
+  {
+    name: "Barbell Biceps Curl",
+    muscleGroup: "Arms",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164832.png",
+  },
+  {
+    name: "Lateral Raises",
+    muscleGroup: "Shoulders",
+    type: "Strength",
+    imageUrl: "/exercisesImages/Captura de pantalla 2025-12-04 164844.png",
+  },
 ];
 
 async function seed() {
