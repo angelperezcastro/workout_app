f15060cb2ddbd5b8f68881b28eb6d8d477febea2
Angel <aperezcastro@safareyes.es>
2025-12-26 06:08:57 +0100

    Updated frontend, deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-})
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: false, // false pq no uso cookies
+});
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
