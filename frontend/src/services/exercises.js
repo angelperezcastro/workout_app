83a0496a729dae630daf75d7e565bfd702ae5170
Angel <aperezcastro@safareyes.es>
2026-01-09 14:22:00 +0100

    Updated exercises

--- frontend/src/services/exercises.js
+++ frontend/src/services/exercises.js
@@ -4,3 +4,8 @@ export async function getExercises() {
   const res = await api.get("/exercises");
   return res.data;
 }
+
+export async function createExercise({ name, muscleGroup = "", type = "", imageUrl = "" }) {
+  const res = await api.post("/exercises", { name, muscleGroup, type, imageUrl });
+  return res.data;
+}
